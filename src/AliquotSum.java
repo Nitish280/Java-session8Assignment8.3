340ac5d81497457c39603cfcbf0afb354248960c
Nitish280 <nsrccs@gmail.com>
2017-06-26 15:38:08 +0530

    aliquot

---
+++ src/AliquotSum.java
@@ -0,0 +1,40 @@
+
+public class AliquotSum {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//here we are taking the aliquot sum of
+		int aliquotSumOf = 12;
+		long aliquotSum = findAliquotSum(aliquotSumOf);
+		
+		System.out.println("Aliquot Sum of " + aliquotSumOf + " is " + aliquotSum + ".");
+		//here we applying the if condition
+		if(aliquotSum == aliquotSumOf){
+			System.out.println(aliquotSumOf + " is a perfect number.");
+		}else{
+			System.out.println(aliquotSumOf + " is not a perfect number.");
+		}
+		
+		//Perfect numbers between 1 to 100
+		System.out.println("Perfect Numbers between 1 to 100 are: ");
+		//here we are intialising the number between 1 to 100
+		for(int i=1; i<=100; i++){
+			aliquotSumOf = i;
+			if(findAliquotSum(i) == aliquotSumOf){
+				System.out.print(aliquotSumOf + " ");
+			}
+		}
+	}
+//here we are creating the another method for finding the aliquot sum
+	public static long findAliquotSum(int n) {
+		long result = 0;
+
+		for (byte i = 1; i <= (n / 2); i++) {
+			if (n % i == 0) {
+				result += i;
+			}
+		}
+		return result;
+	}
+
+}
+
